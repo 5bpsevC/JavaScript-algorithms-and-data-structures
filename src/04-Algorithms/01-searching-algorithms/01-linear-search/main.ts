afefdd2211d1608c50159eaa06bc86d6a8ee3be2
5bpsevC <brite6001@gmail.com>
2025-09-04 01:02:15 +0000

    binary search

---
+++ src/04-Algorithms/01-searching-algorithms/01-linear-search/main.ts
@@ -0,0 +1,37 @@
+// Linear Search Exercise
+// Write a function called linearSearch which accepts an array and a value, and returns the index at which the value exists.
+// If the value does not exist in the array, return -1.
+// Don't use indexOf to implement this function!
+
+function linearSearch(num: number[], value: number) {
+  for (let index = 0; index < num.length; index++) {
+    if (num[index] === value) {
+      return index;
+    }
+  }
+  return -1;
+}
+
+const res = linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4); // 5
+console.log(res);
+
+const americaCities = [
+  "New York 🗽",
+  "Los Angeles 🌴",
+  "Chicago 🌆",
+  "Houston 🤠",
+  "Miami 🌞",
+  "Seattle ☔",
+  "Denver 🏔️",
+  "Boston 🎓",
+  "Phoenix 🌵",
+  "Las Vegas 🎰",
+];
+
+const searchCity = (cities: string[], city: string) => {
+  for (let index = 0; index < cities.length; index++) {
+    if (cities[index] === city) return cities[index];
+  }
+  return -1;
+};
+console.log(searchCity(americaCities, "Phoenix 🌵")); // Phoenix 🌵
